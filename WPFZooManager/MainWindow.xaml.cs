2f84a5c5f4935ddc5a901f87d360aa73f87a7475
Brady Baker <bradysamuelbaker@gmail.com>
2024-02-29 15:09:55 -0800

    Finished Program, DZoo, AZoo, RAnimal, UZoo, DAnimal AAnimalToZoo

--- WPFZooManager/MainWindow.xaml.cs
+++ WPFZooManager/MainWindow.xaml.cs
@@ -3,7 +3,9 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
 
 namespace WPFZooManager {
     /// <summary>
@@ -17,6 +19,7 @@ namespace WPFZooManager {
             string connectionString = ConfigurationManager.ConnectionStrings["WPFZooManager.Properties.Settings.ZooDBConnectionString"].ConnectionString;
             sqlConnection = new SqlConnection(connectionString);
             DisplayZooList();
+            DisplayAnimalList();
         }
 
         private void DisplayZooList() {
@@ -38,15 +41,18 @@ namespace WPFZooManager {
             }
         }
         private void DisplayAssociatedAnimalList() {
+            object zooId = ZooList.SelectedValue;
+            if (zooId == null) {
+                return;
+            }
             try {
-                string query = "select * from Animal a inner join ZooAnimal za on a.Id = za.AnimalId where za.ZooId = @ZooId";
+                string query = "select * from Animal a inner join ZooAnimal za on a.Id = za.AnimalId where za.ZooId = " + zooId;
 
                 SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
 
                 SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
 
                 using (sqlDataAdapter) {
-                    sqlCommand.Parameters.AddWithValue("@ZooId", ZooList.SelectedValue);
                     DataTable animalTable = new DataTable();
 
                     sqlDataAdapter.Fill(animalTable);
@@ -61,9 +67,157 @@ namespace WPFZooManager {
             }
         }
 
+        private void DisplayAnimalList() {
+            try {
+                string query = "select * from Animal";
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter) {
+                    DataTable animalTable = new DataTable();
+
+                    sqlDataAdapter.Fill(animalTable);
+
+                    AnimalList.DisplayMemberPath = "Name";
+                    AnimalList.SelectedValuePath = "Id";
+                    AnimalList.ItemsSource = animalTable.DefaultView;
+                }
+            }
+            catch (Exception e) {
+                MessageBox.Show(e.Message);
+            }
+        }
+
 
         private void ZooList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) {
+            object zooId = ZooList.SelectedValue;
+            if (zooId == null) {
+                return;
+            }
+            NameInput.Text = ((DataRowView)ZooList.SelectedItem)["Location"].ToString();
             DisplayAssociatedAnimalList();
         }
+
+
+        private async void AddAnimalToZoo_Clicked(object sender, RoutedEventArgs ev) {
+            object zooId = ZooList.SelectedValue;
+            object animalId = AnimalList.SelectedValue;
+            if (zooId == null || animalId == null) {
+                MessageBox.Show("Please select a zoo and a animal first");
+                return;
+            }
+            string insertQuery = $"INSERT INTO ZooAnimal (ZooId, AnimalId) VALUES ({zooId}, {animalId})";
+
+            SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+
+            try {
+                sqlConnection.Open();
+                await insertCommand.ExecuteNonQueryAsync();
+                DisplayAssociatedAnimalList();
+                sqlConnection.Close();
+            }
+            catch (Exception e) {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private async void DeleteZoo_Clicked(object sender, RoutedEventArgs e) {
+            object zooId = ZooList.SelectedValue;
+            if (zooId == null) {
+                MessageBox.Show("Please select a Zoo");
+                return;
+            }
+            await UpdateTable("Delete from Zoo Where Id = " + zooId);
+            DisplayZooList();
+            AssociatedAnimalsList.ItemsSource = null;
+        }
+
+
+        private async void DeleteAnimal_Clicked(object sender, RoutedEventArgs e) {
+            object animalId = AnimalList.SelectedValue;
+            if (animalId == null) {
+                MessageBox.Show("Please select a Animal");
+                return;
+            }
+            await UpdateTable("Delete from Animal Where Id = " + animalId);
+            DisplayAnimalList();
+
+        }
+
+
+        private async void RemoveAnimal_Clicked(object sender, RoutedEventArgs e) { //Removes animal from zoo
+            object animalId = AssociatedAnimalsList.SelectedValue;
+            if (animalId == null) {
+                MessageBox.Show("Please select a Animal");
+                return;
+            }
+            string deleteQuery = "Delete from ZooAnimal Where AnimalId = " + animalId;
+            await UpdateTable(deleteQuery);
+            DisplayAssociatedAnimalList();
+        }
+
+
+        private async void AddZoo_Clicked(object sender, RoutedEventArgs e) {
+            string zooLocation = NameInput.Text;
+            if (!string.IsNullOrEmpty(zooLocation)) {
+                string addQuery = $"Insert into Zoo (Location) Values ('{zooLocation}')";
+                await UpdateTable(addQuery);
+                DisplayZooList();
+            } else {
+                MessageBox.Show("Please input a zoo location");
+            }
+        }
+
+        private async void AddAnimal_Clicked(object sender, RoutedEventArgs e) {
+            string animalName = NameInput.Text;
+            if (!string.IsNullOrEmpty(animalName)) {
+                string addQuery = $"Insert into Animal (Name) Values ('{animalName}')";
+                await UpdateTable(addQuery);
+                DisplayAnimalList();
+            } else {
+                MessageBox.Show("Please input a animal name");
+            }
+        }
+
+        private async void UpdateZoo_Clicked(object sender, RoutedEventArgs e) {
+            string zooLocation = NameInput.Text;
+            object zooId = ZooList.SelectedValue;
+
+            if (!string.IsNullOrEmpty(zooLocation) && zooId != null) {
+                string updateQuery = $"Update Zoo Set Location = '{zooLocation}' Where Id = {zooId}";
+                await UpdateTable(updateQuery);
+                DisplayZooList();
+            } else {
+                MessageBox.Show("Please input a zoo location and choose a zoo");
+            }
+        }
+
+        private async void UpdateAnimal_Clicked(object sender, RoutedEventArgs e) {
+            object newAnimalId = AnimalList.SelectedValue;
+            object curAnimalId = AssociatedAnimalsList.SelectedValue;
+            object zooId = ZooList.SelectedValue;
+            if (newAnimalId != null && curAnimalId != null && zooId != null) {
+               string updateQuery = $"Update ZooAnimal Set AnimalId = '{newAnimalId}' Where ZooId={zooId} And AnimalId={curAnimalId}";
+                await UpdateTable(updateQuery);
+                DisplayAssociatedAnimalList();
+            } else {
+                MessageBox.Show("Please choose a zoo, a associated animal, and a animal from the animal list");
+            }
+        }
+
+
+        private async Task UpdateTable(string query) {
+            try {
+                SqlCommand command = new SqlCommand(query, sqlConnection);
+
+                sqlConnection.Open();
+                await command.ExecuteNonQueryAsync();
+                sqlConnection.Close();
+            } catch (Exception ex) {
+                MessageBox.Show("Error updating table: " + ex.Message);
+                sqlConnection.Close();
+            }
+        }
+
     }
 }
